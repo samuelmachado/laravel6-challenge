f844ce88e482bb0a80e4751282280d029f44fbf4
samuelmachado <samuel.silva39@fatec.sp.gov.br>
2020-07-22 06:42:08 -0300

    Layout + blade - part 2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -49,5 +49,8 @@ Released   : 20140225
 	</div>
 </div>
 @yield('content')
+<div id="copyright" class="container">
+	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
 </body>
 </html>
