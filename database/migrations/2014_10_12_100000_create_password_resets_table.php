3f0736cd7649a75d5d0b2feebe2c616b69bf2b71
samuelmachado <samuel.silva39@fatec.sp.gov.br>
2020-07-23 06:32:22 -0300

    first commit auth

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAssignmentsTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateAssignmentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('assignments', function (Blueprint $table) {
-            $table->id();
-            $table->text('body');
-            $table->boolean('completed_at')->default(false);
-            $table->timestamps();
-            $table->timestamp('due_date')->nullable();
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -29,6 +27,6 @@ class CreateAssignmentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('assignments');
+        Schema::dropIfExists('password_resets');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Article;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,36 +11,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/ 
+*/
 
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
-    
-    return view('about', [ 
-        'articles' =>  App\Article::take(2)->latest()->get()
-    ]);
-});
-Route::get('/', function () {
-   
-    return view('test', [
-        'name' => request('name')
-    ]);
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/posts/{post}', 'PostsController@show');
-Route::get('/articles/create', 'ArticlesController@create');
-Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
-Route::get('/articles', 'ArticlesController@index')->name('articles.index');
-Route::post('/articles', 'ArticlesController@store');
-Route::post('/articles', 'ArticlesController@store');
-Route::get('/articles/{article}/edit', 'ArticlesController@edit');
-Route::put('/articles/{article}', 'ArticlesController@update');
-
-
-// /articles
-// /articles /
